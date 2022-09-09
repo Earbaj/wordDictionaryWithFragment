cff15cd4d797ba69f0488d0751560a9e717d1938
Earbaj <earbajsaria3@gmail.com>
2022-09-09 10:23:18 +0600

    Impleent both list and word andpter

--- app/src/main/java/com/example/worddictionarywithfragment/adapter/LetterListAdapter.kt
+++ app/src/main/java/com/example/worddictionarywithfragment/adapter/LetterListAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.worddictionarywithfragment.adapter
 
-class LetterListAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class LetterListAdapter: RecyclerView.Adapter<LetterListAdapter.LetterViewHolder>(){
+
+    class LetterViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LetterViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/example/worddictionarywithfragment/adapter/WordListadapter.kt
+++ app/src/main/java/com/example/worddictionarywithfragment/adapter/WordListadapter.kt
@@ -1,4 +1,24 @@
 package com.example.worddictionarywithfragment.adapter
 
-class WordListadapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class WordListadapter: RecyclerView.Adapter<WordListadapter.WordViewHolder>(){
+
+    class WordViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
